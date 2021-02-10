df348d1f4b44a874e1c41b9fe4519166e56130d5
dead monkey <5215503ren@163.com>
2021-02-10 08:09:17 +0800

    usart 串口程序

--- Basic/usart/usart.c
+++ Basic/usart/usart.c
@@ -81,7 +81,7 @@ void uart_init(u32 bound){
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
     USART_Init(USART1, &USART_InitStructure); //初始化串口
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启ENABLE/关闭DISABLE中断
+    USART_ITConfig(USART1, USART_IT_RXNE, DISABLE);//开启ENABLE/关闭DISABLE中断
     USART_Cmd(USART1, ENABLE);                    //使能串口 
 }
 
--- User/main.c
+++ User/main.c
@@ -1,89 +1,26 @@
 #include "stm32f10x.h"
 #include "sys.h"
 #include "delay.h"	
-#include "led.h"  	
-#include "key.h"     	
-#include "flash.h"
-#include "buzzer.h"
 #include "usart.h"
 
-#define FLASH_START_ADDR  0x0801f000	  //写入的起始地址
-#define Baud_Rate  115200	  //写入的起始地址
+#define BAUD_RATE 115200	  //写入的起始地址
 
 int main (void)
-{			 
-	// 亮暗类型
-	u8 lightDarkType;
-
-	//调节呼吸灯的占空比
-	u16 dutyCycle, i, a;
-
+{			
+    u8 a;
+	 
 	// 初始化配置
     RCC_Configuration();
 
-	// 初始化LED灯驱动
-	LED_Init();
+	uart_init(BAUD_RATE);
 
-	// 初始化KEY驱动
-	KEY_Init();
-
-	uart_init(Baud_Rate);
-
-	BUZZER_Init();
-	//BUZZER_BEEP1();
-	MIDI_PLAY();
-
-	//从指定页的地址读FLASH
-	a = FLASH_R(FLASH_START_ADDR);
-
-	//直接数值操作将变量值写入LED（LED在GPIOB组的PB0和PB1上）
-	GPIO_Write(LEDPORT,a); 
-	lightDarkType = 0;
-    dutyCycle = 1;
 	while(1)
 	{
-	    // USART_SendData(USART1 , 0x55);
-		while(USART_GetFlagStatus(USART1, USART_FLAG_TC)==RESET);
-	    if(!GPIO_ReadInputDataBit(KEYPORT,KEY1)){ //读按键接口的电平
-            delay_ms(20); //延时去抖动
-            if(!GPIO_ReadInputDataBit(KEYPORT,KEY1)){ //读按键接口的电平
-                GPIO_WriteBit(LEDPORT,LED1,(BitAction)(0)); //LED取反
-                GPIO_WriteBit(LEDPORT,LED2,(BitAction)(0)); //LED取反
-            }
-			BUZZER_BEEP1();
-			delay_us(2000);
-			BUZZER_BEEP1();
-			while(!GPIO_ReadInputDataBit(KEYPORT,KEY1));
-        }
-		if(!GPIO_ReadInputDataBit(KEYPORT,KEY2))
+        if (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) != RESET)
 		{
-		    delay_ms(20); //延时去抖动
-			BUZZER_BEEP1();
+		    a = USART_ReceiveData(USART1);
+			printf("%c", a);
 		}
-		if (lightDarkType == 0)
-		{
-		    for (i = 0; i < 10; i++)
-		    {
-		        LED_Breathing_Lamp(dutyCycle);
-		    }
-		    dutyCycle++;
-		    if (dutyCycle == 500)
-		    {
-		        lightDarkType = 1;    
-		    }
-		 }
-		 if (lightDarkType == 1)
-		 {
-		     for (i = 0; i < 10; i++)
-		     {
-		         LED_Breathing_Lamp(dutyCycle);
-		     }
-		     dutyCycle--;
-		     if (dutyCycle == 1)
-		     {
-		         lightDarkType = 0;    
-		     }
-		 }
 	}
 }
 
